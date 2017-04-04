8f12ad2cf75d90d321acc5153c339039c99eb91f
fujiapples21 <kx10976@deforestschools.org>
2017-04-04 08:49:57 -0500

    Add files via upload

---
+++ Global texts.py
@@ -0,0 +1,9 @@
+#text global
+a = []
+b=raw_input('What time do you have work from?')
+def schedule(raw_input):
+    global a
+    routine = 'schedule' and 'today'
+    if routine in (raw_input):
+        print('you have work from', b)
+    a = a + b
