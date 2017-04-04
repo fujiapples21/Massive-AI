77ebd13d7b1e23c9ffd00afb7d10ceaf8a137bd9
fujiapples21 <kx10976@deforestschools.org>
2017-04-04 08:50:40 -0500

    Delete Global texts.py

--- Global texts.py
+++
@@ -1,9 +0,0 @@
-#text global
-a = []
-b=raw_input('What time do you have work from?')
-def schedule(raw_input):
-    global a
-    routine = 'schedule' and 'today'
-    if routine in (raw_input):
-        print('you have work from', b)
-    a = a + b
